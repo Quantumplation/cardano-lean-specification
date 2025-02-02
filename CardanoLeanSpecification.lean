d394c27aa6cbc2213394d3f8db88af4f02065f0f
Pi Lanningham <pi.lanningham@gmail.com>
2025-02-02 13:17:45 -0500

    Fix import

--- CardanoLeanSpecification.lean
+++ CardanoLeanSpecification.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `CardanoLeanSpecification` library.
 -- Import modules here that should be built as part of the library.
-import CardanoLeanSpecification.Basic
+import CardanoLeanSpecification.Cardano
